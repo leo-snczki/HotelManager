7dc6b90f8fd5c2466addd2d020297ce7059d141f
Rafael Belchior <L2474@alunos.epbjc.pt>
2024-05-03 17:09:41 +0100

    Added some variables just to start

--- HotelManager/Program.cs
+++ HotelManager/Program.cs
@@ -10,7 +10,11 @@ namespace HotelManager
     {
         static void Main(string[] args)
         {
-
+            //o programa irá fazer o gerenciamento de 15 quartos
+            uint[] price = new uint[15]; //vetor para o preço de cada quarto
+            string[] qrooms = new string[15]; //vetor para a qualidade do quarto
+            bool[] booked = new bool[15]; //vetor para endicar se o quarto está ocupado ou não
+            DateTime[,] time = new DateTime[15, 2]; // matriz para endicar quanto tempo um quero vai ficar ocupado
         }
     }
 }
